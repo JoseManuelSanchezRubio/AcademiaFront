9142f18124b690aa1e269488c3e0552a7d245fa9
JoseManuel <josemanuelsanchezrubio@gmail.com>
2023-05-30 13:09:42 +0200

    forum css done

--- src/routes/Forum.jsx
+++ src/routes/Forum.jsx
@@ -1,10 +1,84 @@
 import { useEffect, useState } from "react";
 import Nav from "../Nav";
+//import assets
 import send from "../assets/send.png";
+import nothing from "../assets/nothing.png";
 
 
 export default function Forum() {
   const [messages, setMessages] = useState([]);
+  /* const messages = [
+    {
+      id: 1,
+      body: "ghostwhite",
+      user_id: 1,
+      created_at: "01/01/2000",
+      user: { id: 1, name: "Carlos", surname: "Martínez" },
+    },
+    {
+      id: 2,
+      body: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+      user_id: 3,
+      created_at: "01/01/2000",
+      user: { id: 3, name: "Pedro", surname: "López" },
+    },
+    {
+      id: 3,
+      body: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+      user_id: 2,
+      created_at: "01/01/2000",
+      user: { id: 2, name: "María", surname: "Sánchez" },
+    },
+    {
+      id: 4,
+      body: "lorem ipsum lorem ipsum lorem ipsum",
+      user_id: 1,
+      created_at: "01/01/2000",
+      user: { id: 1, name: "Carlos", surname: "Martínez" },
+    },
+    {
+      id: 5,
+      body: "aliceblue",
+      user_id: 1,
+      created_at: "01/01/2000",
+      user: { id: 1, name: "Carlos", surname: "Martínez" },
+    },
+    {
+      id: 6,
+      body: "lorem ipsum lorem ipsum lorem ipsum",
+      user_id: 2,
+      created_at: "01/01/2000",
+      user: { id: 2, name: "María", surname: "Sánchez" },
+    },
+    {
+      id: 7,
+      body: "lorem ipsum",
+      user_id: 4,
+      created_at: "01/01/2000",
+      user: { id: 4, name: "Ana", surname: "Rodríguez" },
+    },
+    {
+      id: 8,
+      body: "lorem ipsum lorem ipsum lorem ipsum",
+      user_id: 1,
+      created_at: "01/01/2000",
+      user: { id: 1, name: "Carlos", surname: "Martínez" },
+    },
+    {
+      id: 9,
+      body: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+      user_id: 1,
+      created_at: "01/01/2000",
+      user: { id: 1, name: "Carlos", surname: "Martínez" },
+    },
+    {
+      id: 10,
+      body: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+      user_id: 2,
+      created_at: "01/01/2000",
+      user: { id: 2, name: "María", surname: "Sánchez" },
+    },
+  ]; */
   const [message, setMessage] = useState("");
   const forum_id = window.location.pathname.split("/")[3];
   const userId = JSON.parse(sessionStorage.getItem("user")).id;
@@ -48,54 +122,71 @@ export default function Forum() {
   return (
     <div>
       <Nav isLogged={isLogged}></Nav>
-      <div className="container pt-3">
-        <h1>Mensajes del foro</h1>
-        <div>
-          {messages.length == 0 && (
-            <div className="mb-2 text-secondary">
-              Todavía no hay ningún mensaje
+      <div className="container">
+        <div className="py-3">
+          <h1 className="mb-4">Mensajes del foro</h1>
+          <div className="forum-background rounded p-3">
+            <div>
+              {messages.length == 0 && (
+                <div className="no-messages">
+                  <img src={nothing} width="200px"/>
+                  <br></br>
+                  <label className="py-4">Vaya... parece que todavía no hay ningún mensaje...</label>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-        <div>
-          {messages.map((message) => (
-            <div
-              key={message.id}
-              className={message.user_id == userId ? "text-end" : ""}
-            >
-              {message.body} ({message.user.name}) ({message.created_at})
+            <div>
+              {messages.map((message) => (
+                <div
+                  key={message.id}
+                  className={
+                    message.user_id == userId
+                      ? "msg-right m-1 p-2"
+                      : "msg-left m-1 p-2"
+                  }
+                >
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="fw-bold">
+                      {message.user.name} {message.user.surname}
+                    </div>
+                    <div className="small">{message.created_at}</div>
+                  </div>
+                  <div className="float-start">{message.body}</div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <form>
-          <div className="form-group d-flex">
-            <input
-              type="text"
-              className="form-control me-2 rounded-pill"
-              id="msg"
-              placeholder="Mensaje"
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            {message == "" ? (
-              <button
-                type="submit"
-                className="rounded-circle btn py-2 btn"
-                disabled
-                onClick={(e) => saveMessage(e)}
-              >
-                <img src={send} width="18px"></img>
-              </button>
-            ) : (
-              <button
-                type="submit"
-                className="rounded-circle py-2 btn border border-dark"
-                onClick={(e) => saveMessage(e)}
-              >
-                <img src={send} width="18px"></img>
-              </button>
-            )}
+            <div style={{ clear: "both" }}></div>
           </div>
-        </form>
+          <form>
+            <div className="form-group d-flex pt-4">
+              <input
+                type="text"
+                className="form-control me-2 rounded-pill"
+                id="msg"
+                placeholder="Mensaje"
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              {message == "" ? (
+                <button
+                  type="submit"
+                  className="rounded-circle btn py-2 btn border"
+                  disabled
+                  onClick={(e) => saveMessage(e)}
+                >
+                  <img src={send} width="18px"></img>
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  className="rounded-circle py-2 btn border border-dark"
+                  onClick={(e) => saveMessage(e)}
+                >
+                  <img src={send} width="18px"></img>
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
